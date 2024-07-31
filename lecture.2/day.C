c09bd56c423636d628aa9c4865d55d122454499e
Bhupendra Patil <bhupendrakp2008@gmail.com>
2024-07-31 16:34:26 +0530

    Add files via upload

---
+++ lecture.2/day.C
@@ -0,0 +1,11 @@
+// bio data 
+#include<stdio.h>
+#include<conio.h>
+main()
+ {
+ 	printf("Name: Bhupendra .k. Patil\n");
+ 	printf("Address: 13, karm Residency , Dindoli , Surat\n");
+ 	printf("E-Mail: bhupendrakp08@gmail.com\n");
+ 	printf("Contact No. : 78887471030\n");
+ 	printf("Privious School: Vedant International\n");
+ }
