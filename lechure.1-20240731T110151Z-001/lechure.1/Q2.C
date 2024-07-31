8506fd41532607e78e3c440aef6d9d1c82e2ed2b
Bhupendra Patil <bhupendrakp2008@gmail.com>
2024-07-31 16:34:04 +0530

    Add files via upload

---
+++ lechure.1-20240731T110151Z-001/lechure.1/Q2.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	printf("  --------\n");
+	printf(" |        |\n");
+	printf(" R        |\n");
+	printf(" N        |\n");
+	printf(" W        |\n");
+    printf(" |        |\n");
+	printf("  --------\n");
+}
